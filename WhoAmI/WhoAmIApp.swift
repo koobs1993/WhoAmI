5a36e18d339e129d49e7a6280192def46710febf
koobs1993 <kelkyleley1993@gmail.com>
2024-12-15 15:40:39 +0800

    Initial Commit

---
+++ WhoAmI/WhoAmIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoAmIApp.swift
+//  WhoAmI
+//
+//  Created by Kyle Kelley on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WhoAmIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhoAmITests/WhoAmITests.swift
@@ -0,0 +1,16 @@
+//
+//  WhoAmITests.swift
+//  WhoAmITests
+//
+//  Created by Kyle Kelley on 12/15/24.
+//
+
+import Testing
+
+struct WhoAmITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
